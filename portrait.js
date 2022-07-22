d37971895b6619509c1e81336306ada6d7ef64eb
tugrule <tugruledurdu@gmail.com>
2022-07-22 17:50:03 -0400

    Added portfolio project

---
+++ portrait.js
@@ -0,0 +1,8 @@
+function toggleMail() {
+    var mail = document.getElementById("gmail");
+    if (mail.style.display === "none") {
+      mail.style.display = "block";
+    } else {
+      mail.style.display = "none";
+    }
+  }
